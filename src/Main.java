ee4802cb2329216fec2866fc31d0542c84d1d317
Yermek Tairkhan <trouzerop@gmail.com>
2024-07-19 22:04:21 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int first = (1+1);
+        int second = first + 3 * ( 2+5);
+
+        first = 5;
+
+        int third = first + second ;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
+    }
+}
