f8fb0945f1a48c32e232657c0c8569cbbf1c7167
Yermek Tairkhan <trouzerop@gmail.com>
2024-07-21 13:43:53 +0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,11 @@ public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give a year: ");
-        int leap = Integer.parseInt(scanner.nextLine());
-        if(leap % 4 != 0){
-            System.out.println("the year is not a leap year.");
-        }
-        else if (leap %100 == 0 && leap %400 ==0 ){
-            System.out.println("the year is leap year ");
-        }
-        else if (leap % 4 == 0){
-            System.out.println("the year is leap year");
-        }
-        else {
-            System.out.println("they year is not leap year");
+        int year = scanner.nextInt();
+        if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+            System.out.println(year +" is a leap year " );
+        }else{
+            System.out.println("it is not");
         }
     }
 }
