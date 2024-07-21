3b12b9f43c20394aa47f3bd10aac7462b8cc2864
Yermek Tairkhan <trouzerop@gmail.com>
2024-07-21 13:28:28 +0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        int first = (1+1);
-        int second = first + 3 * ( 2+5);
-
-        first = 5;
-
-        int third = first + second ;
-        System.out.println(first);
-        System.out.println(second);
-        System.out.println(third);
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a year: ");
+        int leap = Integer.parseInt(scanner.nextLine());
+        if(leap % 4 != 0){
+            System.out.println("the year is not a leap year.");
+        }
+        else if (leap %100 == 0 && leap %400 ==0 ){
+            System.out.println("the year is leap year ");
+        }
+        else if (leap % 4 == 0){
+            System.out.println("the year is leap year");
+        }
+        else {
+            System.out.println("they year is not leap year");
+        }
     }
 }
